d68a4aade87c0ee684505e46f7a4243314c3d35e
jorgel <jorgelcch17@gmail.com>
2022-11-15 01:26:30 -0400

    añadiendo juegos a la vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,27 @@
     <div class="lg:py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @livewire('banner-home')
-            <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-4">
+            <h2 class="pt-6">Recargas mediante ID's:</h2>
+            <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
                 <a class="bg-white border-2 border-blue-200 p-2 hover:shadow-lg" href="{{ route('freefire.index') }}">
                     <img class="w-full aspect-square" src="{{ asset('image/freefire.webp') }}" alt="freefire">
                     <span class="block font-bold text-center pt-2">Free Fire</span>
+                </a>
+                <a class="bg-white border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
+                    <img class="w-full aspect-square" src="{{ asset('image/mobile_legend.png') }}" alt="mobile legend">
+                    <span class="block font-bold text-center pt-2">Mobile Legend</span>
+                </a>
+            </div>
+
+            <h2 class="pt-6">Recargas en cuenta directa:</h2>
+            <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
+                <a class="bg-white border-2 border-gray-700 p-2 hover:shadow-lg" href="{{ route('freefire.index') }}">
+                    <img class="w-full aspect-square" src="{{ asset('image/cod_mobile.png') }}" alt="freefire">
+                    <span class="block font-bold text-center pt-2">CoD Mobile</span>
+                </a>
+                <a class="bg-white border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
+                    <img class="w-full aspect-square" src="{{ asset('image/stumble_guys.webp') }}" alt="mobile legend">
+                    <span class="block font-bold text-center pt-2">Stumble Guys</span>
                 </a>
             </div>
         </div>
