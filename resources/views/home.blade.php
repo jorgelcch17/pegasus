ca408756c6414443190f79feebfb7776fc074845
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-19 03:42:26 -0400

    color bg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,15 @@
     <div class="lg:py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @livewire('banner-home')
+
             <h2 class="pt-6">Recargas mediante ID's:</h2>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
-                <a class="bg-white border-2 border-blue-200 p-2 hover:shadow-lg" href="{{ route('freefire.index') }}">
+                <a class="bg-slate-700 border-2 border-blue-200 p-2 hover:shadow-lg" href="{{ route('freefire.index') }}">
                     <img class="w-full aspect-square" src="{{ asset('image/freefire.webp') }}" alt="freefire">
                     <span class="block font-bold text-center pt-2">Free Fire</span>
                 </a>
-                <a class="bg-white border-2 border-yellow-600 p-2 hover:shadow-lg" href="{{ route('mobile-legend.index') }}">
+                <a class="bg-slate-700 border-2 border-yellow-600 p-2 hover:shadow-lg"
+                    href="{{ route('mobile-legend.index') }}">
                     <img class="w-full aspect-square" src="{{ asset('image/mobile_legend.png') }}" alt="mobile legend">
                     <span class="block font-bold text-center pt-2">Mobile Legend</span>
                 </a>
@@ -22,22 +24,34 @@
 
             <h2 class="pt-6">Recargas en cuenta directa:</h2>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
-                <a class="bg-white border-2 border-gray-700 p-2 hover:shadow-lg" href="{{ route('freefire.index') }}">
+                <a class="bg-slate-700 border-2 border-gray-700 p-2 hover:shadow-lg" href="{{ route('freefire.index') }}">
                     <img class="w-full aspect-square" src="{{ asset('image/cod_mobile.png') }}" alt="freefire">
                     <span class="block font-bold text-center pt-2">CoD Mobile</span>
                 </a>
-                <a class="bg-white border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
+                <a class="bg-slate-700 border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
                     <img class="w-full aspect-square" src="{{ asset('image/stumble_guys.webp') }}" alt="mobile legend">
                     <span class="block font-bold text-center pt-2">Stumble Guys</span>
                 </a>
             </div>
             <h2 class="pt-6">Servicio Multimedia:</h2>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
-                <a class="bg-white border-2 border-gray-700 p-2 hover:shadow-lg" href="">
+                <a class="bg-slate-700 border-2 border-gray-700 p-2 hover:shadow-lg" href="">
                     <img class="w-full aspect-square" src="{{ asset('image/netflix.png') }}" alt="Netflix">
                     <span class="block font-bold text-center pt-2">Netflix</span>
                 </a>
-                <a class="bg-white border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
+                <a class="bg-slate-700 border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
+                    <img class="w-full aspect-square" src="{{ asset('image/amazon_prime.png') }}" alt="Amazon prime">
+                    <span class="block font-bold text-center pt-2">Amazon prime</span>
+                </a>
+            </div>
+
+            <h2 class="pt-6">Servicio Multimedia:</h2>
+            <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
+                <a class="bg-slate-700 border-2 border-gray-700 p-2 hover:shadow-lg" href="">
+                    <img class="w-full aspect-square" src="{{ asset('image/netflix.png') }}" alt="Netflix">
+                    <span class="block font-bold text-center pt-2">Netflix</span>
+                </a>
+                <a class="bg-slate-700 border-2 border-yellow-600 p-2 hover:shadow-lg" href="">
                     <img class="w-full aspect-square" src="{{ asset('image/amazon_prime.png') }}" alt="Amazon prime">
                     <span class="block font-bold text-center pt-2">Amazon prime</span>
                 </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="{{ asset('css/splide-default.min.css') }}">
     </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    <body class="font-sans antialiased text-white">
+        
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-slate-800">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
