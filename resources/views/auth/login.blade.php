02f7f8fb314dec2147a5790b1ec415d542f47a7b
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-19 01:55:54 -0400

    login v.3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,14 @@
 <x-guest-layout>
     <section class="h-screen">
         <div class="px-6 h-full text-gray-800">
-            <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-min g-6">
+            <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-screen g-6">
                 <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-0 md:mb-0">
-                    <img src="{{ asset('image/logo-negro.png')}}"
-                        class="w-full" alt="Sample image" />
+                    <a href="/">
+                        <img src="{{ asset('image/logo-negro.png') }}" class="w-full" alt="Logo" />
+                    </a>
                 </div>
                 <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-                    <form>
+                    <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="flex flex-row items-center justify-center lg:justify-start">
                             <p class="text-lg mb-0 mr-4">Inicia sesión con</p>
@@ -48,9 +49,8 @@
                         </div>
                         <!-- Email input -->
                         <div class="mb-6">
-                            <for="email" value="{{ __('Email') }}" />
-                            <input id="email"
-                                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            <label for="email" value="{{ __('Email') }}" />
+                            <input id="email" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 type="email" name="email" :value="old('email')" required autofocus
                                 placeholder="email" />
                         </div>
@@ -76,15 +76,16 @@
                         </div>
 
                         <div class="text-center lg:text-left">
-                            <button type="button"
-                                class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-                                Iniciar sesión
+                            <button class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                                {{ __('INICIAR SESIÓN') }}
                             </button>
+                            @if (Route::has('password.request'))
                             <p class="text-sm font-semibold mt-2 pt-1 mb-0">
-                                ¿No tienes Cuenta?
-                                <a href="#!"
+                                {{ __('¿No tienes una cuenta?') }}
+                                <a href="{{ route('password.request') }}"
                                     class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out">Regístrate</a>
                             </p>
+                            @endif
                         </div>
                     </form>
                 </div>
