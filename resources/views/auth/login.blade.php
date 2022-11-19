57a447e415e0a525b31a21c7d0db3101a945acd3
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-19 01:18:02 -0400

    login v.2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,11 @@
 <x-guest-layout>
     <section class="h-screen">
         <div class="px-6 h-full text-gray-800">
-          <div
-            class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
-          >
-            <div
-              class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
-            >
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                class="w-full"
-                alt="Sample image"
-              />
-            </div>
+            <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+                <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+                    <img src="{{ asset('image/logo-negro.png')}}"
+                        class="w-full" alt="Sample image" />
+                </div>
                 <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
                     <form>
                         @csrf
