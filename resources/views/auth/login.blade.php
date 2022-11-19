3b57a727977df9082bdb43d86072d841b7b952a1
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-19 01:26:37 -0400

    login v.3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <section class="h-screen">
         <div class="px-6 h-full text-gray-800">
-            <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-                <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+            <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-min g-6">
+                <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-0 md:mb-0">
                     <img src="{{ asset('image/logo-negro.png')}}"
                         class="w-full" alt="Sample image" />
                 </div>
