b9d3f0fa35d74dffb2df3ef85f8462bf282bd5ff
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-21 18:00:17 -0400

    DB free fire items

--- resources/views/livewire/freefire-index.blade.php
+++ resources/views/livewire/freefire-index.blade.php
@@ -29,222 +29,55 @@
                     SUDAMERICA</p>
             </div>
             <div class="p-2 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
-                <div class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-tarjeta-mensual.png') }}"
-                            alt="free fire tarjeta mensual" />
-                    </a>
-                    <div class="p-3">
+                @foreach ($items as $item)
+                    <div
+                        class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">Tarjeta mensual</h5>
+                            <img class="rounded-t-lg" src="{{ asset('image') }}/{{ $item->image }}"
+                                alt="{{ $item->name }}" />
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
+                        <div class="p-3">
+                                <label for="item-{{ $item->id }}">
+                                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">{{ $item->name }} {{ $item->bonus }}</h5>
+                                </label>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs
+                                20.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs
+                                {{ $item->price }} </p>
+                            <a href="{{ route('freefire.detail', $item) }}"
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Comprar Ahora
+                                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
+                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
                     </div>
-                </div>
-
-                <div
-                    class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-tarjeta-semanal.png') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Tarjeta semanal</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-diamantes.jpg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">100 + 10 diamantes</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <div
-                    class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-diamantes.jpg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">310 + 31 Diamantes</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <div
-                    class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-diamantes.jpg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">520 + 52 Diamantes</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <div
-                    class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-diamantes.jpg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">1050 + 105 Diamantes</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <div
-                    class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-diamantes.jpg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">2160 + 216 Diamantes</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <div
-                    class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="{{ asset('image/free-fire-diamantes.jpg') }}"
-                            alt="" />
-                    </a>
-                    <div class="p-3">
-                        <a href="#">
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">5580 + 558 Diamantes</h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block line-through">Bs 20.</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 inline-block">Bs 12</p>
-                        <a href="#"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Comprar Ahora
-                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
+                @endforeach
             </div>
-            {{-- </div> --}}
-
-        </div>
-        <div class="p-2">
-            <a href="#"
-                class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sobre FREE FIRE</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Es el último juego de disparos de supervivencia
-                    disponible en el móvil. Cada juego de 10 minutos te pondrá en una isla remota con otros 49 jugadores
-                    buscando sobrevivir. Los jugadores podrán elegir con libertad su punto de partida y usar su
-                    paracaídas e intentar mantenerse en la zona segura el mayor tiempo posible. Podrán conducir
-                    vehículos para explorar el mapa, crear estrategias para esconderse o usar objetos para camuflajearse
-                    en el pasto. Embosca y caza, solo hay un objetivo: sobrevivir.</p>
-                <p class="font-normal text-gray-700 dark:text-gray-400">LOS DIAMANTES son los recursos más valiosos en Free Fire.
-                    Úsalos para comprar artículos en la Tienda, activar el Pase Élite, girar en Diamante Royale, comprar
-                    personajes, actualizar personajes y desbloquear habilidades de personajes.</p>
-            </a>
+            <div class="p-2">
+                <a href="#"
+                    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sobre FREE FIRE
+                    </h5>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">Es el último juego de disparos de
+                        supervivencia
+                        disponible en el móvil. Cada juego de 10 minutos te pondrá en una isla remota con otros 49
+                        jugadores
+                        buscando sobrevivir. Los jugadores podrán elegir con libertad su punto de partida y usar su
+                        paracaídas e intentar mantenerse en la zona segura el mayor tiempo posible. Podrán conducir
+                        vehículos para explorar el mapa, crear estrategias para esconderse o usar objetos para
+                        camuflajearse
+                        en el pasto. Embosca y caza, solo hay un objetivo: sobrevivir.</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">LOS DIAMANTES son los recursos más valiosos
+                        en Free Fire.
+                        Úsalos para comprar artículos en la Tienda, activar el Pase Élite, girar en Diamante Royale,
+                        comprar
+                        personajes, actualizar personajes y desbloquear habilidades de personajes.</p>
+                </a>
+            </div>
         </div>
     </div>
-</div>
