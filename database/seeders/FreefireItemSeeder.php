0fbac37db09c9a1e43faaf5a00c376d26f2646e7
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-21 18:51:32 -0400

    agregando seed

--- database/seeders/FreefireItemSeeder.php
+++ database/seeders/FreefireItemSeeder.php
@@ -18,50 +18,50 @@ class FreefireItemSeeder extends Seeder
     public function run()
     {
         DB::table('freefire_items')->insert([
-            'name' => 'Diamond × 100',
+            'name' => 'Diamantes: 100',
             'slug' => Str::slug('Diamond × 100'),
             'bonus' => '+ Bonus 10',
-            'image' => 'freefire.webp',
+            'image' => 'free-fire-diamantes.jpg',
             'price' => '1',
             'is_active' => true,
         ]);
         DB::table('freefire_items')->insert([
-            'name' => 'Diamond × 310',
+            'name' => 'Diamantes × 310',
             'slug' => Str::slug('Diamond × 310'),
             'bonus' => '+ Bonus 31',
-            'image' => 'freefire.webp',
+            'image' => 'free-fire-diamantes.jpg',
             'price' => '3',
             'is_active' => true,
         ]);
         DB::table('freefire_items')->insert([
-            'name' => 'Diamond × 520',
+            'name' => 'Diamantes × 520',
             'slug' => Str::slug('Diamond × 520'),
             'bonus' => '+ Bonus 52',
-            'image' => 'freefire.webp',
+            'image' => 'free-fire-diamantes.jpg',
             'price' => '5',
             'is_active' => true,
         ]);
         DB::table('freefire_items')->insert([
-            'name' => 'Diamond × 1050',
+            'name' => 'Diamantes × 1050',
             'slug' => Str::slug('Diamond × 1050'),
             'bonus' => '+ Bonus 105',
-            'image' => 'freefire.webp',
+            'image' => 'free-fire-diamantes.jpg',
             'price' => '10',
             'is_active' => true,
         ]);
         DB::table('freefire_items')->insert([
-            'name' => 'Diamond × 2160',
+            'name' => 'Diamantes × 2160',
             'slug' => Str::slug('Diamond × 2160'),
             'bonus' => '+ Bonus 216',
-            'image' => 'freefire.webp',
+            'image' => 'free-fire-diamantes.jpg',
             'price' => '20',
             'is_active' => true,
         ]);
         DB::table('freefire_items')->insert([
-            'name' => 'Diamond × 5580',
+            'name' => 'Diamantes × 5580',
             'slug' => Str::slug('Diamond × 5580'),
             'bonus' => '+ Bonus 558',
-            'image' => 'freefire.webp',
+            'image' => 'free-fire-diamantes.jpg',
             'price' => '50',
             'is_active' => true,
         ]);
@@ -69,7 +69,7 @@ class FreefireItemSeeder extends Seeder
             'name' => 'Tarjeta Semanal',
             'slug' => 'tarjeta-semanal',
             'bonus' => '',
-            'image' => 'tarjeta-semanal.jpg',
+            'image' => 'free-fire-tarjeta-semanal.png',
             'price' => '2',
             'is_active' => true,
         ]);
@@ -77,7 +77,7 @@ class FreefireItemSeeder extends Seeder
             'name' => 'Tarjeta Mensual',
             'slug' => 'tarjeta-mensual',
             'bonus' => '',
-            'image' => 'tarjeta-mensual.jpg',
+            'image' => 'free-fire-tarjeta-mensual.png',
             'price' => '10',
             'is_active' => true,
         ]);
