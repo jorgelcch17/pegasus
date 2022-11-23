0b4ec04fdc05d34795f446b26f8bd86d2ed46b65
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-23 17:47:59 -0400

    body css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="{{ asset('css/splide-default.min.css') }}">
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased ">
         
 
-        <div class="min-h-screen bg-slate-800">
+        <div class="min-h-screen bg-slate-800 bg-sky-100">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
@@ -35,7 +35,7 @@
             @endif
 
             <!-- Page Content -->
-            <main class="bg-sky-100">
+            <main>
                 {{ $slot }}
             </main>
         </div>
