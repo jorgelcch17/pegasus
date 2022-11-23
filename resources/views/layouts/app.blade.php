3c00c6a8b3dd6653debe4f3a5b4a1c78a88c8d82
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-23 17:51:00 -0400

    body v.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <body class="font-sans antialiased ">
         
 
-        <div class="min-h-screen bg-slate-800 bg-sky-100">
+        <div class="min-h-screen bg-slate-800">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
