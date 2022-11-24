5ab4980774901e2705e64c20a997530ea1879551
bladimirLopez <bladimirbalderramalopez123@gmail.com>
2022-11-24 00:35:05 -0400

    home img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,40 +5,44 @@
         </h2> 
     </x-slot> --}}
     @livewire('banner-home')
-    <div class="lg:py-0 p-2">
+    <div class="lg:py-1 p-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
             <h2 class="pt-1 font-bold text-xl">Recargas mediante ID</h2>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
                 <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
                     href="{{ route('freefire.index') }}">
-                    <img class="w-full aspect-square" src="{{ asset('image/free-fire-index.jpg') }}" alt="freefire">
-                    <span class="block font-bold text-center pt-2 text-black">Garena Free Fire Diamantes</span>
+                    <img class="rounded-t-lg" src="{{ asset('image/free-fire-index.png') }}" alt="mobile legend">
+                    <span class="block font-bold text-center text-black">Free Fire Diamantes</span>
                 </a>
                 <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
                     href="{{ route('mobile-legend.index') }}">
-                    <img class="w-full aspect-square" src="{{ asset('image/mobile_legend.png') }}" alt="mobile legend">
-                    <span class="block font-bold text-center pt-2 text-black">Mobile Legend</span>
+                    <img class="rounded-t-lg" src="{{ asset('image/mobile_legend.png') }}" alt="mobile legend">
+                    <span class="block font-bold text-center text-black">Mobile Legend</span>
                 </a>
 
                 <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-                    href="{{route('mobile-legend.index') }}">
-                    <img class="w-full aspect-square" src="{{ asset('image/pugb-mobile-index.jpg') }}" alt="mobile legend">
+                    href="{{ route('mobile-legend.index') }}">
+                    <img class="rounded-t-lg"" src="{{ asset('image/pugb-mobile-index.jpg') }}" alt="mobile legend">
                     <span class="block font-bold text-center pt-2 text-black">Pugb Mobile</span>
+                </a>
+
+                <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+                    href="{{ route('freefire.index') }}">
+                    <img class="rounded-t-lg"" src="{{ asset('image/call-of-duty-mobile.png') }}" alt="freefire">
+                    <span class="block font-bold text-center pt-2 text-black">CoD Mobile</span>
+                </a>
+
+                <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+                    href="">
+                    <img class="rounded-t-lg"" src="{{ asset('image/stumble_guys.webp') }}" alt="mobile legend">
+                    <span class="block font-bold text-center pt-2 text-black">Stumble Guys</span>
                 </a>
             </div>
 
             <h2 class="pt-6 font-bold text-xl">Recargas en cuenta directa</h2>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
-                <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-                    href="{{ route('freefire.index') }}">
-                    <img class="w-full aspect-square" src="{{ asset('image/cod_mobile.png') }}" alt="freefire">
-                    <span class="block font-bold text-center pt-2 text-black">CoD Mobile</span>
-                </a>
-                <a class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-                    href="">
-                    <img class="w-full aspect-square" src="{{ asset('image/stumble_guys.webp') }}" alt="mobile legend">
-                    <span class="block font-bold text-center pt-2 text-black">Stumble Guys</span>
-                </a>
+
+
             </div>
             <h2 class="pt-6 font-bold text-xl">Servicio Multimedia</h2>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-2 gap-2">
